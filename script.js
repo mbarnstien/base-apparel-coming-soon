04537475094e688722b5bf27c9ce976c79035a17
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-05-03 12:47:34 -0700

    Corrected Validate Email Function

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 const button = document.querySelector('.button');
-const input = document.getElementById('email').value;
+const inputText = document.querySelector('input');
 const errorIcon = document.querySelector('.error');
 const errorMessage = document.querySelector('.error-message');
 const cardInput = document.querySelector('.card-input');
 
-function checkEmail(input) {
-    let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (input.match(validRegex)) {
-        return (true)
-    }
-    return (false)
-};
-
-button.addEventListener('click', function (e) {
-    e.preventDefault();
-    console.log('click!')
-    if (input === "") {
-        errorIcon.classList.remove('hide');
-        errorMessage.classList.remove('hide');
-        cardInput.classList.add('error-border');
-
-    } else if (!checkEmail(input)) {
+const validateEmail = function () {
+    const mailformat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+    if (inputText.value.match(mailformat)) {
+        errorIcon.classList.add('hide');
+        errorMessage.classList.add('hide');
+        cardInput.classList.remove('error-border');
+      return true;
+    } else if (inputText === "") {
         errorIcon.classList.remove('hide');
         errorMessage.classList.remove('hide');
         cardInput.classList.add('error-border');
     } else {
-        errorIcon.classList.add('hide');
-        errorMessage.classList.add('hide');
-        cardInput.classList.remove('error-border');
+        errorIcon.classList.remove('hide');
+        errorMessage.classList.remove('hide');
+        cardInput.classList.add('error-border');
+        return false;
     }
+  };
+
+button.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log('click!')
+    validateEmail();
 });
